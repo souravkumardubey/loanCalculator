b44ff49cc88528d5b11a71305851ab21283ac0b3
shubhamexpo <skd8238@gmail.com>
2019-07-17 14:14:33 +0530

    added loading features

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
-document.getElementById('loan-form').addEventListener('submit',calculateResults);
+document.getElementById('loan-form').addEventListener('submit',function(e){
 
-function calculateResults(e){
+    document.getElementById('results').style.display='none';
+
+    document.getElementById('loading').style.display = 'block';
+
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+});
+
+function calculateResults(){
 
     console.log('calculating...');
 
@@ -33,14 +42,23 @@ function calculateResults(e){
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly*calculatedPayments)-principal).toFixed(2);
+
+        document.getElementById('results').style.display='block';
+
+        document.getElementById('loading').style.display='none';
     }else{
         showError('please check your numbers')
     }
 
-    e.preventDefault();
+    
 }
 
 function showError(error){
+    document.getElementById('results').style.display='none';
+
+    document.getElementById('loading').style.display='none';
+
+    
 
     const card = document.querySelector('.card');
     const heading = document.querySelector('.heading');
