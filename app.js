61fb4e0d6c1d87fa1c5ae86b042d4882b03d97ad
shubhamexpo <skd8238@gmail.com>
2019-07-17 13:59:53 +0530

    added error landing

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ document.getElementById('loan-form').addEventListener('submit',calculateResults)
 
 function calculateResults(e){
 
-    console.log('calculation');
+    console.log('calculating...');
 
-    const UIamount = document.getElementById('amount');
+    const amount = document.getElementById('amount');
 
     const interest = document.getElementById('interest');
 
@@ -18,15 +18,45 @@ function calculateResults(e){
 
 
 
-    const principal = parseFloat('amount.value');
+    const principal = parseFloat(amount.value);
 
     const calculatedIntrest = parseFloat(interest.value)/100/12;
 
     const calculatedPayments = parseFloat(years.value) * 12;
 
 
-    const x = Math.pow(1+ calculatedIntrest + calculatedPayments);
+    const x = Math.pow(1+ calculatedIntrest , calculatedPayments);
 
+    const monthly = (principal*x*calculatedIntrest)/(x-1);
+    
+    if(isFinite(monthly)){
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly*calculatedPayments)-principal).toFixed(2);
+    }else{
+        showError('please check your numbers')
+    }
 
-    e.preventDefaut();
+    e.preventDefault();
+}
+
+function showError(error){
+
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+
+    const errorDiv = document.createElement('div');
+
+    errorDiv.className = 'alert alert-danger';
+
+    errorDiv.appendChild(document.createTextNode(error));
+
+    card.insertBefore(errorDiv,heading);
+
+    setTimeout(clearError, 2000);
+
+}
+
+function clearError(){
+    document.querySelector('.alert').remove();
 }
